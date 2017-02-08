9a2b1ac7fe863873569307b9d4dd9646df9d385b
AbinayaPonmalar <abinayaponmalar7@gmail.com>
2017-02-08 23:03:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
 var img = document.getElementById('madi');
-img.onClick = function() {
-    img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onClick = function () {
+   var interval = setInterval(moveRight, 100);
 };
