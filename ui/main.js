8b56ae2ee53efdddfc91469fcfe1aace7a164ff7
AbinayaPonmalar <abinayaponmalar7@gmail.com>
2017-02-10 22:07:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,7 @@ button.onclick = function(){
     request.open('GET', 'http://abinayaponmalar.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
